8a7bf2e99d13515fd67aa4f5543c510c6e106e7e
Hasnain <Hasnainkhankqs@gmail.com>
2017-02-01 11:58:41 +0500

    adding Eproject semister1

--- AptechProject/sign_in_out/app.js
+++ AptechProject/sign_in_out/app.js
@@ -7,19 +7,18 @@ var mail = document.getElementsByTagName("input")[4];
 var sign = document.getElementById("signupbtn");
 // localStorage.setItem("email",mail);
 // localStorage.setItem("password",psword);
-sign.addEventListener("click",function(){
- var nm =  nam.value;
- var fn =  fname.value;
- var cntct =  contact.value;
- var pswrd =  psword.value;
- var mal =  mail.value;
- if(nm.length > 1 && fn.length > 1 && cntct.length > 1 && pswrd.length > 1 && mal.length > 1){
-    localStorage.setItem("email",mal);
-    localStorage.setItem("pswrd",psword);
-    window.location = "page3.html";
- }
-})
-
+// sign.addEventListener("click",function(){
+//  var nm =  nam.value;
+//  var fn =  fname.value;
+//  var cntct =  contact.value;
+//  var pswrd =  psword.value;
+//  var mal =  mail.value;
+//  if(nm.length > 1 && fn.length > 1 && cntct.length > 1 && pswrd.length > 1 && mal.length > 1){
+//     localStorage.setItem("email",mal);
+//     localStorage.setItem("pswrd",psword);
+//     window.location = "page3.html";
+//  }
+// })
 
 
 // var attemp = 3;
